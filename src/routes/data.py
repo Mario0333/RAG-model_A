84bb21c134b658b4c14d30114f3dcea772d7871b
00JIMMY00 <jimmygamaljimmy@gmail.com>
2024-11-10 18:26:43 +0200

    return file_id

--- src/routes/data.py
+++ src/routes/data.py
@@ -28,7 +28,7 @@ async def upload_data(project_id: str, file: UploadFile, app_settings: Settings 
         )
 
     project_dir_path = ProjectController().get_project_path(project_id)
-    file_path = DataController().generate_unique_filename(
+    file_path, file_id = DataController().generate_unique_filepath(
         orig_file_name=file.filename,
         project_id=project_id
     )
@@ -49,6 +49,7 @@ async def upload_data(project_id: str, file: UploadFile, app_settings: Settings 
     return JSONResponse(
             status_code=status.HTTP_200_OK,
             content={
-                "signal":ResponseSignal.FILE_UPLOAD_SUCCESS.value
+                "signal":ResponseSignal.FILE_UPLOAD_SUCCESS.value,
+                "file_id":file_id
             }
         )
