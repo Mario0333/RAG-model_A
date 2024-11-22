8205e537a480d61d58f8a46df00703c606e6546a
00JIMMY00 <jimmygamaljimmy@gmail.com>
2024-11-22 21:07:58 +0200

    some updates

--- src/helpers/config.py
+++ src/helpers/config.py
@@ -1,18 +1,18 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
 class Settings(BaseSettings):
-
     APP_NAME: str
     APP_VERSION: str
     OPENAI_API_KEY: str
 
     FILE_ALLOWED_TYPES: list
     FILE_MAX_SIZE: int
-    
+
     FILE_DEFAULT_CHUNK_SIZE: int
 
-    MONGODG_URL = str
-    MONGODG_DATABASE = str
+    MONGODG_URL: str # Correct usage of ClassVar for class-level variables
+    MONGODG_DATABASE: str       # Correctly annotate this as a regular field
+
     class Config:
         env_file = ".env"
 
