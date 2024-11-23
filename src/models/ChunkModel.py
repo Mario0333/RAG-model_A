6b8228c9f8cb9c64664a2e1adb6c45f3f8970f91
00JIMMY00 <jimmygamaljimmy@gmail.com>
2024-11-23 12:23:13 +0200

    insert_many_chunks

---
+++ src/models/ChunkModel.py
@@ -0,0 +1,43 @@
+from .BaseDataModel import BaseDataModel
+from .db_schemes import DataChunk
+from .enums.DataBaseEnum import DataBaseEnum
+from bson.objectid import ObjectId
+from pymongo import InsertOne
+
+class ChunkModel(BaseDataModel):
+    
+    def __init__(self, db_client):
+        super().__init__(db_client)
+        self.collection = self.db_client[DataBaseEnum.COLLECTION_CHUNK_NAME.value]
+
+    async def create_chunk(self, chunk:DataChunk):
+        result = await self.collection.insert_one(chunk.model_dump(by_alias=True, exclude_unset=True))
+        chunk._id = result.inserted_id
+        return chunk
+    
+    async def get_chunk(self, chunk_id:str):
+        result = self.collection.find_one({
+            "_id":ObjectId(chunk_id)
+        })
+
+        if result == None :
+            return None
+        
+        return DataChunk(**result)
+    
+    async def insert_many_chunks(self, chunks:list , batch_size : int=100):
+
+        for i in range(0,len(chunks), batch_size):
+            batch = chunks[i:i+batch_size]
+
+            operations = [
+                InsertOne(document=chunk)
+                for chunk in batch
+            ]
+
+            await self.collection.bulk_write(operations)
+
+        return len(chunks)
+
+    
+    
